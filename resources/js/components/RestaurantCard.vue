b5e0d76858bcd84e8c99afeb028222b2e64c5f8c
Gozzissimo <fantini.nico@gmail.com>
2022-04-01 17:49:43 +0200

    ReastaurantCard new Component

---
+++ resources/js/components/RestaurantCard.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="col-sm-12 col-md-6 col-lg-3 p-0">
+        <a class="text-white text-center card m-2" href="#">
+            <img v-if="restaurantImg" class="card-img h-100 w-100" :src="require(`../../../public/storage/${restaurantImg}`)" :alt="restaurantName">
+            <img v-else :src="require(`../../../public/storage/default_images/default-placeholder.png`)" alt="default-image">
+            <div class="card-img-overlay h-100 posision-relative">
+                <h5 class="card-title position-absolute top-50 start-50 translate-middle">{{ restaurantName }}</h5>
+            </div>
+        </a>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "RestaurantCard",
+    props: {
+        restaurantName: {
+            type: String,
+        },
+        restaurantImg: {
+            type: String,
+        },
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+    // RESTAURANT CARD
+    .card {
+        height: 150px;
+        min-width: 250px;
+        overflow: hidden;
+        border-radius: 20px;
+        box-shadow: 0 0 12px 0 rgba(0, 0, 0, 0.2);
+        &:hover,
+        &:focus,
+        &:active {
+            .card-img {
+                transform: scale(1.1);
+                transition: transform .3s ease-in-out;
+            }
+            .card-img-overlay {
+                background: none;
+        }
+        }
+        .card-img {
+            object-fit: cover;
+        }
+        .card-img-overlay {
+            background: linear-gradient(
+                0deg,
+                rgba(0, 0, 0, 0.2) 0%,
+                rgba(0, 0, 0, 0.3) 100%
+            );
+        }
+        .card-title {
+            font-weight: bold;
+            font-size: 1.5em;
+        }
+    }
+</style>
--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,25 +1,25 @@
 <template>
 <div class="">
     <div class="row">
-        <div class="col-sm-12 col-md-6 col-lg-3 p-0"
+        <RestaurantCard
             v-for="(restaurant, index) in filteredRestaurants"
-            :key="index">
-            <a class="text-white text-center card m-2" href="">
-                <img class="card-img h-100 w-100" :src=" require('../../../public/storage/uploads/pD2otbMppEzcCrCmf2eGotDwJp2X3o9lVgXMa7xP.jpg')" alt="foto-esempio">
-                <div class="card-img-overlay h-100 posision-relative">
-                    <h5 class="card-title position-absolute top-50 start-50 translate-middle">{{ restaurant.restaurant_name }}</h5>
-                </div>
-            </a>
-        </div>
+            :key="index"
+            :restaurantName = "restaurant.restaurant_name"
+            :restaurantImg = "restaurant.image"
+        />
     </div>
 </div>
 </template>
 
 <script>
 import Axios from "axios";
+import RestaurantCard from '../components/RestaurantCard.vue';
 
 export default {
     name: "Home",
+    components: {
+        RestaurantCard,
+    },
     // PRENDO TRAMITE LE PROPS L'ARRAY CATEGORIES DALLA PAGINA APP.VUE
     props: {
         categoriesArray: Array,
@@ -33,7 +33,7 @@ export default {
     },
     created() {
         axios
-            // CHIAMATA AXION PER PRENDERE I DATI DALLA ROTTA IN API.PHP
+            // CHIAMATA AXIOS PER PRENDERE I DATI DALLA ROTTA IN API.PHP
             .get("http://127.0.0.1:8000/api/restaurants/data", {
                 headers: {
                     // UTILIZZIAMO UN BEARER TOKEN INVIANDOLO COME STRINGA E CI AGGIUNGIAMO LA CHIAVE API
@@ -46,6 +46,7 @@ export default {
                 // SE LA LUNGHEZZA DELL'ARRAY FILTRATO è 0 CI PUSHO TUTTI I RISTORANTI (ALTRIMENTI AVREI PAGINA VUOTA SEMPRE)
                 if (this.filteredRestaurants.length == 0) {
                     this.filteredRestaurants.push(...this.restaurants);
+                    console.log(this.restaurants);
                 }
             })
             .catch((err) => {
@@ -78,37 +79,4 @@ export default {
 
 <style lang="scss" scoped>
 
-    // RESTAURANT CARD
-    .card {
-        height: 150px;
-        min-width: 250px;
-        overflow: hidden;
-        border-radius: 20px;
-        box-shadow: 0 0 12px 0 rgba(0, 0, 0, 0.2);
-        &:hover,
-        &:focus,
-        &:active {
-            .card-img {
-                transform: scale(1.1);
-                transition: transform .3s ease-in-out;
-            }
-            .card-img-overlay {
-                background: none;
-        }
-        }
-        .card-img {
-            object-fit: cover;
-        }
-        .card-img-overlay {
-            background: linear-gradient(
-                0deg,
-                rgba(0, 0, 0, 0.2) 0%,
-                rgba(0, 0, 0, 0.3) 100%
-            );
-        }
-        .card-title {
-            font-weight: bold;
-            font-size: 1.5em;
-        }
-    }
 </style>

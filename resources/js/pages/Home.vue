443fc2c64f383547799b66ed3290c3ebaf51b4ac
Gozzissimo <fantini.nico@gmail.com>
2022-04-01 11:41:57 +0200

    Card fix dashboards.scss

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,11 +1,11 @@
 <template>
 <div class="">
     <div class="row">
-        <div class="col-sm-12 col-md-6 col-lg-3"
+        <div class="col-sm-12 col-md-6 col-lg-3 p-0"
             v-for="(restaurant, index) in filteredRestaurants"
             :key="index">
-            <a class="text-white card p-0 m-2" href="">
-                <img class="card-img" :src=" require('../../../public/storage/uploads/BXo64grHE7Zei04qTUv32Y3W575UUpg0B95Ei11x.jpg')" alt="foto-esempio">
+            <a class="text-white text-center card m-2" href="">
+                <img class="card-img h-100 w-100" :src=" require('../../../public/storage/uploads/pD2otbMppEzcCrCmf2eGotDwJp2X3o9lVgXMa7xP.jpg')" alt="foto-esempio">
                 <div class="card-img-overlay h-100 posision-relative">
                     <h5 class="card-title position-absolute top-50 start-50 translate-middle">{{ restaurant.restaurant_name }}</h5>
                 </div>
@@ -76,27 +76,39 @@ export default {
 };
 </script>
 
-<style lang="scss">
-.card {
-    min-height: 0;
-    height: 150px;
-    min-width: 260px !important;
-    &:hover,
-    &:focus,
-    &:active {
+<style lang="scss" scoped>
+
+    // RESTAURANT CARD
+    .card {
+        height: 150px;
+        min-width: 250px;
+        overflow: hidden;
+        border-radius: 20px;
+        box-shadow: 0 0 12px 0 rgba(0, 0, 0, 0.2);
+        &:hover,
+        &:focus,
+        &:active {
+            .card-img {
+                transform: scale(1.1);
+                transition: transform .3s ease-in-out;
+            }
+            .card-img-overlay {
+                background: none;
+        }
+        }
         .card-img {
-            transform: scale(1.1);
-            transition: transform .3s;
+            object-fit: cover;
+        }
+        .card-img-overlay {
+            background: linear-gradient(
+                0deg,
+                rgba(0, 0, 0, 0.2) 0%,
+                rgba(0, 0, 0, 0.3) 100%
+            );
+        }
+        .card-title {
+            font-weight: bold;
+            font-size: 1.5em;
         }
     }
-    .card-img {
-        width: 100%;
-        height: 100%;
-        object-fit: cover;
-    }
-    .card-title {
-        font-weight: bold;
-        font-size: 1.5em;
-    }
-}
 </style>

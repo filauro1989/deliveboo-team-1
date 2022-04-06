a8e0b0f8a0273cbbf49bafa21d2fe92b8c3c57f2
filauro1989 <filauro1989@gmail.com>
2022-04-06 15:23:56 +0200

    redirect from paymentAccepted

--- resources/js/pages/PaymentAccepted.vue
+++ resources/js/pages/PaymentAccepted.vue
@@ -1,10 +1,17 @@
 <template>
-    <div>PIPPOANKIO</div>
+    <div class="container d-flex justify-content-center align-items-center">
+        <div>PAGAMENTO AVVENUTO CON SUCCESSO</div>
+    </div>
 </template>
 
 <script>
 export default {
     name: "PaymentAccepted",
+    created() {
+        setTimeout(() => {
+            this.$router.push("/");
+        }, 5000);
+    },
 };
 </script>
 

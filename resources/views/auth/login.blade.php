7a7bd3cfa99384d2e9f3cd591861efee637fc26e
Gtp87 <fabio.giannitrapani@gmail.com>
2022-04-08 18:04:44 +0200

    fix login.blade

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,8 +4,11 @@
     <div class="container-login mt-0">
         <div class="top-login"></div>
         <div class="bottom-login"></div>
-        <div class="center-login">
-            <div class="fs-3">{{ __('Login') }}</div>
+        <div class="center-login mb-5">
+            <div class="img-login-container">
+                <img class="login-img" src="{{ asset('/storage/default_images/MyDeliveboo10sBlue-unscreen.gif') }}"
+                    alt="DeliveBoo Logo">
+            </div>
             <form method="POST" action="{{ route('login') }}">
                 @csrf
                 <label for="email">{{ __('E-mail') }}</label>
@@ -34,86 +37,11 @@
                     <a class="btn my_btn-login" href="{{ url('/') }}">
                         {{ __('Indietro') }}
                     </a>
+                    <a class="btn btn-link" href="{{ route('register') }}">
+                        {{ __('Non sei registrato?') }}
+                    </a>
                 </div>
             </form>
-
-
-
-            {{-- <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-mail') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" @error('email') is-invalid @enderror"
-                                    name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password"
-                                class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password"
-                                    class="form-control @error('password') is-invalid @enderror" name="password" required
-                                    autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember"
-                                        {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Ricordami') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn my_btn-login">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ url('/') }}">
-                                        {{ __('Indietro') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div> --}}
-            {{-- <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-md-8">
-                    </div>
-                </div>
-            </div> --}}
-
         </div>
     </div>
 @endsection
